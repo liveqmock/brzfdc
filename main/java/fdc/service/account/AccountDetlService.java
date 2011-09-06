2bbbd8b914dd5762944913929f7adca38beb965a
haiyu117 <haiyu117@cd452dff-0e3b-4f33-b03c-3bd053f628fd>
2011-09-06 05:44:54 +0000

    监管账户交易明细

---
+++ main/java/fdc/service/account/AccountDetlService.java
@@ -0,0 +1,41 @@
+package fdc.service.account;
+
+import fdc.repository.dao.RsAccDetailMapper;
+import fdc.repository.model.RsAccDetail;
+import fdc.repository.model.RsAccDetailExample;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: haiyuhuang
+ * Date: 11-9-6
+ * Time: ÉÏÎç10:15
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class AccountDetlService {
+    @Autowired
+    private RsAccDetailMapper rsAccDetailMapper;
+
+    public List<RsAccDetail> selectedRecordsByTradeDate(Date beginDate,Date endDate) {
+        RsAccDetailExample example = new RsAccDetailExample();
+        if (beginDate != null && endDate != null) {
+            example.createCriteria().andTradeDateBetween(beginDate,endDate);
+        }
+        return rsAccDetailMapper.selectByExample(example);
+    }
+
+    public RsAccDetailMapper getRsAccDetailMapper() {
+        return rsAccDetailMapper;
+    }
+
+    public void setRsAccDetailMapper(RsAccDetailMapper rsAccDetailMapper) {
+        this.rsAccDetailMapper = rsAccDetailMapper;
+    }
+}
---
+++ main/java/fdc/view/account/AccountTradeDetlAction.java
@@ -0,0 +1,77 @@
+package fdc.view.account;
+
+import fdc.repository.model.RsAccDetail;
+import fdc.service.account.AccountDetlService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: haiyuhuang
+ * Date: 11-9-6
+ * Time: ÉÏÎç10:07
+ * To change this template use File | Settings | File Templates.
+ */
+@ManagedBean
+@ViewScoped
+public class AccountTradeDetlAction {
+    private static final Logger logger = LoggerFactory.getLogger(AccountTradeDetlAction.class);
+    @ManagedProperty(value="#{accountDetlService}")
+    private AccountDetlService accountDetlService;
+    private Date beginDate;
+    private Date endDate;
+    private List<RsAccDetail> rsAccDetails;
+
+    @PostConstruct
+    public void init() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.DAY_OF_MONTH,1);
+        beginDate = cal.getTime();
+        endDate = new Date();
+        rsAccDetails = accountDetlService.selectedRecordsByTradeDate(beginDate,endDate);
+    }
+
+    public void onBtnQueryClick() {
+        rsAccDetails = accountDetlService.selectedRecordsByTradeDate(beginDate,endDate);
+    }
+
+    public AccountDetlService getAccountDetlService() {
+        return accountDetlService;
+    }
+
+    public void setAccountDetlService(AccountDetlService accountDetlService) {
+        this.accountDetlService = accountDetlService;
+    }
+
+    public Date getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(Date beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public List<RsAccDetail> getRsAccDetails() {
+        return rsAccDetails;
+    }
+
+    public void setRsAccDetails(List<RsAccDetail> rsAccDetails) {
+        this.rsAccDetails = rsAccDetails;
+    }
+}
